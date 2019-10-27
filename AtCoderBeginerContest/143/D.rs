d5e1990fa98a787a4ac9e755574a510f732738ed
Shinyaigeek <PhilisPaxil@gmail.com>
2019-10-27 13:10:54 +0900

    [set]snippet

---
+++ AtCoderBeginerContest/143/D.rs
@@ -0,0 +1,63 @@
+use std::io::stdin;
+
+mod utils {
+    use std::io::BufRead;
+    use std::str::{self, FromStr};
+
+    pub struct StdinReader<R: BufRead> {
+        reader: R,
+        buf: Vec<u8>,
+    }
+
+    impl<R: BufRead> StdinReader<R> {
+        pub fn new(reader: R) -> StdinReader<R> {
+            StdinReader {
+                reader: reader,
+                buf: Vec::new(),
+            }
+        }
+
+        #[allow(unused)]
+        #[inline]
+        pub fn reads<T: FromStr>(&mut self) -> T {
+            self.read_until(b' ')
+        }
+
+        #[allow(unused)]
+        #[inline]
+        pub fn readl<T: FromStr>(&mut self) -> T {
+            self.read_until(b'\n')
+        }
+
+        #[inline]
+        pub fn read_until<T: FromStr>(&mut self, delim: u8) -> T {
+            loop {
+                self.buf.clear();
+                let len = self.reader
+                    .read_until(delim, &mut self.buf)
+                    .expect("failed reading bytes");
+                match len {
+                    0 => panic!("early eof"),
+                    1 if self.buf[0] == delim => (),
+                    _ => {
+                        if self.buf[len - 1] == delim {
+                            self.buf.truncate(len - 1);
+                        }
+                        break;
+                }
+            }
+        }
+
+        let elem = unsafe { str::from_utf8_unchecked(&self.buf) };
+        elem.parse().unwrap_or_else(|_| panic!(format!("failed parsing: {}", elem)))
+    }
+    }
+}
+
+
+fn main(){
+    let stdin = stdin();
+    let mut r = utils::StdinReader::new(stdin.lock());
+    let n:usize = r.readl();
+    println!("{}",n);
+}
