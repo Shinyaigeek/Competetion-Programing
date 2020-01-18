0aab23d475bf374a9ea2ff62f739806c63a285df
Shinyaigeek <PhilisPaxil@gmail.com>
2020-01-18 18:16:30 +0900

    [handin]

---
+++ LeetCode/base7/main.rs
@@ -0,0 +1,92 @@
+use std::io::stdin;
+use std::vec::Vec;
+
+mod utils {
+    use std::io::BufRead;
+    use std::str::{self, FromStr};
+
+    pub struct StdinReader<R: BufRead> {
+        reader: R,
+        buf: Vec<u8>,
+    }
+
+    impl<R: BufRead> StdinReader<R> {
+        pub fn new(reader: R) -> StdinReader<R> {
+            StdinReader {
+                reader: reader,
+                buf: Vec::new(),
+            }
+        }
+
+        #[allow(unused)]
+        #[inline]
+        pub fn reads<T: FromStr>(&mut self) -> T {
+            self.read_until(b' ')
+        }
+
+        #[allow(unused)]
+        #[inline]
+        pub fn readl<T: FromStr>(&mut self) -> T {
+            self.read_until(b'\n')
+        }
+
+        #[allow(unused)]
+        #[inline]
+        pub fn read_vec<T: FromStr>(&mut self) -> Vec<T> {
+            return self.read_until::<String>(b'\n').trim().split_whitespace().map(|c| c.parse().ok().unwrap()).collect();
+        }
+
+        #[inline]
+        pub fn read_until<T: FromStr>(&mut self, delim: u8) -> T {
+            loop {
+                self.buf.clear();
+                let len = self.reader
+                    .read_until(delim, &mut self.buf)
+                    .expect("failed reading bytes");
+                match len {
+                    0 => panic!("early eof"),
+                    1 if self.buf[0] == delim => (),
+                    _ => {
+                        if self.buf[len - 1] == delim {
+                            self.buf.truncate(len - 1);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            let elem = unsafe { str::from_utf8_unchecked(&self.buf) };
+            elem.parse().unwrap_or_else(|_| panic!(format!("failed parsing: {}", elem)))
+         }
+    }
+}
+
+
+fn main(){
+    let stdin = stdin();
+    let mut r = utils::StdinReader::new(stdin.lock());
+    let num:i32 = r.readl();
+    let mut remaining:i32 = num;
+    let mut ans_n = 0;
+    let mut i = 0;
+    while(true) {
+        if(remaining % 7 == remaining) {
+            ans_n += remaining * pow(10,i);
+            break;
+        }else{
+            let divisionRemainder = remaining % 7;
+            remaining = (remaining - divisionRemainder) / 7;
+            ans_n += divisionRemainder * pow(10,i);
+            i += 1;
+        }
+    }
+    return ans_n.to_string();
+}
+
+fn pow(base:i32, tar:i32) -> i32 {
+    let mut ans = 1;
+    for i in 0..tar {
+        ans *= base;
+    }
+    return ans;
+}
