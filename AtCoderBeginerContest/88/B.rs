63235661ef2ca33589616bc91b5ce0958ac963bc
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-05 23:22:05 +0900

    [abc]88 b

---
+++ AtCoderBeginerContest/88/B.rs
@@ -0,0 +1,86 @@
+use std::io::stdin;
+use std::vec::Vec;
+
+mod utils {
+    use std::io::BufRead;
+    use std::str::{self, FromStr};
+
+    pub struct StdinReader<R: BufRead> {
+        reader: R,
+        buf: Vec<u8>,
+    }
+
+    impl<R: BufRead> StdinReader<R> {
+        pub fn new(reader: R) -> StdinReader<R> {
+            StdinReader {
+                reader: reader,
+                buf: Vec::new(),
+            }
+        }
+
+        #[allow(unused)]
+        #[inline]
+        pub fn reads<T: FromStr>(&mut self) -> T {
+            self.read_until(b' ')
+        }
+
+        #[allow(unused)]
+        #[inline]
+        pub fn readl<T: FromStr>(&mut self) -> T {
+            self.read_until(b'\n')
+        }
+
+        #[allow(unused)]
+        #[inline]
+        pub fn read_vec<T: FromStr>(&mut self) -> Vec<T> {
+            return self.read_until::<String>(b'\n').trim().split_whitespace().map(|c| c.parse().ok().unwrap()).collect();
+        }
+
+        #[inline]
+        pub fn read_until<T: FromStr>(&mut self, delim: u8) -> T {
+            loop {
+                self.buf.clear();
+                let len = self.reader
+                    .read_until(delim, &mut self.buf)
+                    .expect("failed reading bytes");
+                match len {
+                    0 => panic!("early eof"),
+                    1 if self.buf[0] == delim => (),
+                    _ => {
+                        if self.buf[len - 1] == delim {
+                            self.buf.truncate(len - 1);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            let elem = unsafe { str::from_utf8_unchecked(&self.buf) };
+            elem.parse().unwrap_or_else(|_| panic!(format!("failed parsing: {}", elem)))
+         }
+    }
+}
+
+
+fn main(){
+    let stdin = stdin();
+    let mut r = utils::StdinReader::new(stdin.lock());
+    let n:usize = r.readl();
+    let mut a_s:Vec<i16> = r.read_vec();
+
+    a_s.sort();
+    a_s.reverse();
+
+    let mut score_alice = 0;
+    let mut score_bob = 0;
+
+    for i in 0..n {
+        if(i % 2 == 1){
+            score_bob += a_s[i];
+        }else{
+            score_alice += a_s[i];
+        }
+    }
+
+    println!("{}", score_alice - score_bob);
+}
