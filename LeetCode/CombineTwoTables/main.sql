f1b2cf4863df165bda4ce7c372fea85f5f36302d
svpcadmin <PhilisPaxil@gmail.com>
2020-06-21 10:51:24 +0900

    handin

--- LeetCode/CombineTwoTables/main.sql
+++ LeetCode/CombineTwoTables/main.sql
@@ -1,1 +1,6 @@
-SELECT * FROM Address;
+-- SELECT City, Status FROM Address WHERE PersonId IN (
+--     SELECT DISTINCT(PersonId)
+--     FROM Person
+-- );
+
+SELECT FirstName, LastName, City, Status From Address INNER JOIN Person ON Address.PersonId = Person.PersonId;
